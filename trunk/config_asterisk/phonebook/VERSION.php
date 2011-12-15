824133ec30b593ad398ead0fe0455a8bcef1939d
3090607@gmail.com <3090607@gmail.com@87e387c9-e29c-378c-7398-b2daa68b8784>
2011-12-15 17:39:47 +0000

    tmp

---
+++ trunk/config_asterisk/phonebook/VERSION.php
@@ -0,0 +1,14 @@
+<? 
+$file="VERSION";
+if(file_exists($file))
+{
+$fp = fopen($file, "r");
+	while (!feof($fp)) 
+	{
+	 $zeile = fgets($fp,500);
+	 if($zeile>"")echo $zeile;
+	}
+fclose($fp);
+}else{echo "ERROR: kein File";}
+
+?>
